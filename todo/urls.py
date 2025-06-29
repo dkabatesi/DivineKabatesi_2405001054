b294827abd6703b9f399d12ee9d1f9bde1d8f92e
Diva Kabatesi <kabatesidivine@gmail.com>
2025-06-29 18:51:52 +0200

    Add app-level URL patterns to enable routing

--- todo/urls.py
+++ todo/urls.py
@@ -1,1 +1,11 @@
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.login_user, name='login'),
+    path('register/', views.register_user, name='register'),
+    path('logout/', views.logout_user, name='logout'),
+    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
+]
